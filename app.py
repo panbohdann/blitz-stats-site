e455ac5a6da96b0e057acb17181961aa631feb72
Pan Bohdan <79790204+panbohdann@users.noreply.github.com>
2026-02-08 16:12:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,8 @@ def stats():
         "survival_rate": round(survival/battles*100,2) if battles else 0
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
